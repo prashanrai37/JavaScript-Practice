9d72cf64c2c85ec1e3ccce0f2f16c169bc5eeb9d
Prashan <76526590+prashanrai37@users.noreply.github.com>
2022-10-31 17:25:07 +0000

    Create notes.js

---
+++ notes.js
@@ -0,0 +1,14 @@
+// var ->
+// let - for variable values
+// const - for constant values
+
+let age = 22;
+// value of age can change
+
+const name = 'prashan';
+name = 'rai';
+// value of name cannot change
+
+// arrow functions
+const myFnc = () => {};
+const multiply = (age) => age * 2;
