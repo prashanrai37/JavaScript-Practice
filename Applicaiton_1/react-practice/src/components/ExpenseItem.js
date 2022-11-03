54a9f48843b6e0adf55e41bc3d4fdfae4c9ec3f3
Prashan <76526590+prashanrai37@users.noreply.github.com>
2022-11-03 17:44:13 +0000

    Commit

---
+++ Applicaiton_1/react-practice/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>March 28th</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
